d7a7c610ec12a41d622144a055ce496a2d57932b
gsak3l <gsakel5@gmail.com>
2021-02-24 09:47:34 +0200

    imdb, rottentomatoes and metascore scores are being printed

--- data.py
+++ data.py
@@ -1,4 +1,11 @@
 import json
+import pickle
+
+
+from data_clean import minute_to_integer
+from date_conversion import convert_date
+from get_disney_films import scrape_disney_films
+from money_conversion import convert_money
 
 
 def save_data(title, data):
@@ -9,3 +16,35 @@ def save_data(title, data):
 def load_data(title):
     with open(title, encoding='utf-8') as f:
         return json.load(f)
+
+
+def save_json_data():
+    movie_info_list_ = scrape_disney_films()
+    for movie in movie_info_list_:  # new fields
+        movie['Running time (int)'] = minute_to_integer(movie.get('Running time', 'N/A'))  # string to int
+        movie['Budget (float)'] = convert_money(movie.get('Budget', 'N/A'))  # string to float
+        movie['Box office (float)'] = convert_money(movie.get('Box office', 'N/A'))  # string to float
+        movie['Release date (datetime)'] = str(convert_date(movie.get('Release date', 'N/A')))  # string
+    save_data('disney_data_cleaned.json', movie_info_list_)
+
+
+def load_json_data():
+    movie_info_list_ = load_data('disney_data_cleaned.json')
+    return movie_info_list_
+
+
+def save_data_pickle(name, data):
+    movie_info_list_ = scrape_disney_films()
+    for movie in movie_info_list_:  # new fields
+        movie['Running time (int)'] = minute_to_integer(movie.get('Running time', 'N/A'))  # string to int
+        movie['Budget (float)'] = convert_money(movie.get('Budget', 'N/A'))  # string to float
+        movie['Box office (float)'] = convert_money(movie.get('Box office', 'N/A'))  # string to float
+        movie['Release date (datetime)'] = convert_date(movie.get('Release date', 'N/A'))  # string to datetime
+
+    with open(name, 'wb') as f:
+        pickle.dump(data, f)
+
+
+def load_data_pickle(name):
+    with open(name, 'rb') as f:
+        return pickle.load(f)
--- main.py
+++ main.py
@@ -1,41 +1,36 @@
 import json
-import pickle
-from get_disney_films import scrape_disney_films
-from data import save_data, load_data
-from data_clean import minute_to_integer
-from money_conversion import convert_money
-from date_conversion import convert_date
+import requests
+import urllib
+
+from pprint import pprint
+from data import save_data, load_data, save_json_data, load_json_data, save_data_pickle, load_data_pickle
 
 
-def load_json_data():
-    movie_info_list_ = load_data('disney_data_cleaned.json')
-    return movie_info_list_
+def get_omdb_info(title):
+    base_url = 'http://www.omdbapi.com/?'
+    parameters = {'apikey': 'e5a099f9', 't': title}
+    params_encoded = urllib.parse.urlencode(parameters)
+    full_url = base_url + params_encoded
+    return requests.get(full_url).json()
 
 
-def create_json_data():
-    movie_info_list_ = scrape_disney_films()
-    for movie in movie_info_list_:  # new fields
-        movie['Running time (int)'] = minute_to_integer(movie.get('Running time', 'N/A'))  # string to int
-        movie['Budget (float)'] = convert_money(movie.get('Budget', 'N/A'))  # string to float
-        movie['Box office (float)'] = convert_money(movie.get('Box office', 'N/A'))  # string to float
-        movie['Release date (datetime)'] = convert_date(movie.get('Release date', 'N/A'))  # string to datetime
-    save_data_pickle('disney_movie_data_cleaned_more.pickle', movie_info_list_)
-    # save_data('disney_data_cleaned.json', movie_info_list_)
-    # return movie_info_list_
-
-
-def save_data_pickle(name, data):
-    with open(name, 'wb') as f:
-        pickle.dump(data, f)
-
-
-def load_data_pickle(name):
-    with open(name, 'rb') as f:
-        return pickle.load(f)
+def get_rotten_tomatoes_score(omdb_info):
+    ratings = omdb_info.get('Ratings', [])
+    for rating in ratings:
+        if rating['Source'] == 'Rotten Tomatoes':
+            return rating['Value']
+    return None
 
 
 if __name__ == '__main__':
-    # movie_info_list = create_json_data()
-    # create_json_data()
     movie_info_list = load_data_pickle('disney_movie_data_cleaned_more.pickle')
-    print(movie_info_list)
+    # print(json.dumps(get_omdb_info('into the woods'), indent=4, sort_keys=True))
+    info = get_omdb_info('Her')
+    rotten = get_rotten_tomatoes_score(info)
+    for movie in movie_info_list:
+        title = movie['title']
+        omdb_info = get_omdb_info(title)
+        movie['imdb'] = omdb_info.get('imdbRating', None)
+        movie['metascore'] = omdb_info.get('Metascore', None)
+        movie['rotten_tomatoes'] = get_rotten_tomatoes_score(omdb_info)
+    save_data_pickle()
