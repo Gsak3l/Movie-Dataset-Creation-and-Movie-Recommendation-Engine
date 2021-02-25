cba77f51ed18f97717983a51cd144e3b643947e4
gsak3l <gsakel5@gmail.com>
2021-02-25 18:46:50 +0200

    tiny typo

--- main.py
+++ main.py
@@ -9,5 +9,4 @@ from data import save_data, load_data, save_json_data, load_json_data, \
 if __name__ == '__main__':
     save_json_data('disney_movie_data_cleaned_with_scores.json')
     save_data_pickle('disney_movie_data_cleaned_with_scores.pickle')
-    movie_data_set = load_data_pickle('disney_movie_data_cleaned_with_scores.pickle')
-    save_data_csv(movie_data_set, 'disney_movie_data_cleaned_with_scores.csv')
+    save_data_csv('disney_movie_data_cleaned_with_scores.csv')
