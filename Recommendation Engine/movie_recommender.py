49e7d34290515a43175e8f6cb49796a0ef52a091
gsak3l <gsakel5@gmail.com>
2021-03-06 18:03:04 +0200

    movie recomendations work i guess

--- Recommendation Engine/movie_recommender.py
+++ Recommendation Engine/movie_recommender.py
@@ -1,5 +1,7 @@
 import pandas as pd
-
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 def get_title_from_index(index):
@@ -26,3 +28,20 @@ if __name__ == '__main__':
 
     df['combined_features'] = df.apply(combined_features, axis=1)
     print(df['combined_features'])
+
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(df['combined_features'])
+    cosine_sim = cosine_similarity(count_matrix)
+
+    movie_index = get_index_from_title('Avatar')  # aye aye aye, aye aye aye, fuego
+    similar_movies = list(enumerate(cosine_sim[movie_index]))
+
+    sorted_similar_movies = sorted(similar_movies, key=lambda x: x[1], reverse=True)
+
+    i = 0
+    for movie in sorted_similar_movies:
+        if i != 0:
+            print(get_title_from_index(movie[0]))
+        if i >= 10:
+            break
+        i += 1
